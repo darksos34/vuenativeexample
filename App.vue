f6df7f72c0afd26ae5b0df33b02bf4db5fd959de
Your Name <j.hamwijk34@gmail.com>
2021-01-12 16:22:52 +0100

    Commit Vue-native project to github
    1e day project

---
+++ App.vue
@@ -0,0 +1,32 @@
+<template>
+  <view class="container">
+    <text class="text-color-primary"></text>
+
+    <button
+        :on-press="onPressLearnMore"
+        title="Gran Canaria"
+        color="#841584"
+        accessibility-label="Learn more about this purple button"
+    />
+  </view>
+</template>
+<script>
+export default {
+  methods: {
+    onPressLearnMore: function() {
+      alert('Pas op he')
+    }
+  }
+}
+</script>
+<style>
+.container {
+  background-color: white;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+}
+.text-color-primary {
+  color: blue;
+}
+</style>

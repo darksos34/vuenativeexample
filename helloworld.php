1efb45f89957de18d41d7d3775a0586f2791d10f
Your Name <j.hamwijk34@gmail.com>
2021-01-20 18:39:34 +0100

    Variable Functions - Demo
    Plurasight learning course
    PHP Fundamentals  -  Varaible Functions

--- helloworld.php
+++ helloworld.php
@@ -1,16 +1,11 @@
 <?php
 
-define('CHECK_CONSTANT', "Yes, I am a constant!");
+function getAuthor()
+{
+    echo "Mister Coder";
+}
+$variableFunctioName = "getAuthor";
 
-$intVar = 1234;
-$stringVar = "I'm a String";
-$boolVar = false;
-$floatVar = 12.34;
-
-//echo is_string($intVar);
-//echo is_int($floatVar);
-//echo is_bool($boolVar);
-
-echo is_float($floatVar);
+$variableFunctioName();
 
 ?>

7f0cd87c08b6254887f01a4f540cd5930d311033
Your Name <j.hamwijk34@gmail.com>
2021-01-20 19:02:39 +0100

    Create SingleQuotes.php

---
+++ Php/SingleQuotes.php
@@ -0,0 +1,17 @@
+<?php
+
+$valuta = 'cent';
+
+$exampleString = 'A $valuta saved is a $valuta earned.';
+
+//echo $exampleString;
+
+$var = 2;
+
+//echo $var."nd place";
+
+$misc = "Mr Coding\"s Day";
+
+
+echo $misc;
+

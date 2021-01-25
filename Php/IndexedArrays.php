fcb3039f71f9b39d44111aa01485aa1b7157ae89
Your Name <j.hamwijk34@gmail.com>
2021-01-25 12:55:15 +0100

    Create IndexedArrays.php

---
+++ Php/IndexedArrays.php
@@ -0,0 +1,9 @@
+<?php
+
+$authors = array ("Isac Global", "Jordy Clim", "Juli Ratis" );
+$books = ["Harry Potter", "Lost in the Forest", "Clean Code"];
+
+$allTypes = [10, 1.3, "Hello", true];
+print_r($allTypes);
+//print_r($authors);
+?>
